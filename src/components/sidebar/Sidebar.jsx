9e5d073822fc61659f43835e426d31f1066fff23
daxuanzi515 <96161750+daxuanzi515@users.noreply.github.com>
2022-12-22 03:12:58 +0800

    Update Sidebar.jsx

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -29,7 +29,13 @@ export default function Sidebar() {
         <div className="sidebarItem">
             <span className="sidebarTitle">CATEGORIES</span>
             <ul className="sidebarList">
-
+               {
+                cats.map((c)=>(
+                <Link to={`/?cat={c.name}`} className="link">
+                <li className="sidebarListItem">{c.name}</li>
+                </Link>
+               ))
+                }
             </ul>
         </div>
         <div className="sidebarItem">
