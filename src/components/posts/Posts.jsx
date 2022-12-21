d13f6d9120dd313e3c6b7b8df9eb77431b911921
daxuanzi515 <96161750+daxuanzi515@users.noreply.github.com>
2022-12-22 02:17:53 +0800

    Update Posts.jsx

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -5,11 +5,11 @@ export default function Posts({posts}) {
   return (
       <div className="posts">
         {
-    (posts && posts.length >0)?:
+    posts ?:
           (posts.map((c)=>(
           <Post post={c}/>
           )))
-  :(<p>loading</p>)
+  :(<></>)
          }
       </div>
   )
